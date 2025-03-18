7cc2ee4e7fdd59ee6b08e105707a8ee08100d3ac
huylebron <doquochuy27102003@gmail.com>
2025-03-18 09:20:55 +0700

    Add database connection settings and configure persistence services

--- HotelProject.Api/Program.cs
+++ HotelProject.Api/Program.cs
@@ -1,3 +1,5 @@
+using HotelProject . Persistence ;
+
 var builder = WebApplication . CreateBuilder ( args ) ;
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder . Services . AddControllers ( ) ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder . Services . AddEndpointsApiExplorer ( ) ;
 builder . Services . AddSwaggerGen ( ) ;
+
+#region Persistence
+
+builder.Services.AddSqlServerPersistence(builder.Configuration);
+builder.Services.AddRepositoryUnitOfWork();
+
+
+#endregion
+
 
 var app = builder . Build ( ) ;
 
