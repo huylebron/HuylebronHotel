3d4bccf5e186ee0f576f286c728f72d849f85c18
huylebron <doquochuy27102003@gmail.com>
2025-03-20 10:51:25 +0700

    Add AccountController and ImageService for user and image management

---
+++ HotelProject.Domain/Abstractions/ApplicationServices/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using HotelProject . Domain . Model . Commons ;
+using HotelProject . Domain . Model . Images ;
+using HotelProject . Domain . Model . Users ;
+
+namespace HotelProject.Domain.Abstractions.ApplicationServices ;
+
+public interface IImageService
+{
+    Task<PageResult<ImageViewModel>> GetImages(ImageSearchQuery query);
+    Task<ResponseResult> UploadImages(UploadImageViewModel model, UserProfileModel? currentUser = null);
+
+    Task<ResponseResult> UpdateImage(UpdateImageViewModel model, UserProfileModel? currentUser = null);
+    Task<ResponseResult> DeleteImage(Guid imageId);
+
+    Task<ResponseResult> UpdateStatus(UpdateStatusViewModel model);
+}
